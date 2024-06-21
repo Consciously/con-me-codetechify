50cca5b374268292bd66248e9edc6a77e98e1ba1
Consciously <stefanihle@gmx.net>
2024-06-21 18:15:46 +0200

    add animation to profile image with framer-motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,38 @@
+'use client';
+
+import { useState } from 'react';
 import MaxWidthWrapper from '@/components/max-width-wrapper';
+import { motion, AnimatePresence } from 'framer-motion';
 import { Button } from '@/components/ui/button';
-import { ArrowRight } from 'lucide-react';
+import { ArrowLeft, ArrowRight } from 'lucide-react';
 import Image from 'next/image';
+import { generateRange } from '@/lib/utils';
+
+const buttonAnimationVariants = {
+	initial: (i: number) => ({
+		x: i % 2 === 0 ? '-100%' : '100%',
+		opacity: 0,
+	}),
+	animate: (i: number) => ({
+		x: 0,
+		opacity: 1,
+		transition: {
+			delay: i * 0.3,
+			duration: 0.5,
+		},
+	}),
+};
+
+const overlayAnimationVariants = {
+	initial: { opacity: 0 },
+	animate: { opacity: 0.8, transition: { duration: 0.5 } },
+};
+
+const range = generateRange(0, 2);
 
 export default function Home() {
+	const [isHovered, setIsHovered] = useState(false);
+
 	return (
 		<>
 			<section className='relative block lg:min-h-screen'>
@@ -25,24 +54,60 @@ export default function Home() {
 								This is a simple portfolio website built with ~Next.js and
 								Tailwind CSS. It is a work in progress and will be updated
 							</p>
-							<div className='relative mt-12 z-10 overflow-hidden w-fit mx-auto'>
+							<div
+								className='relative mt-12 z-10 w-fit mx-auto'
+								onMouseEnter={() => setIsHovered(true)}
+								onMouseLeave={() => setIsHovered(false)}
+							>
 								<Image
 									src='/images/profile_image.jpg'
 									alt='Profile image from Stefan Ihle'
 									width={552}
 									height={736}
-									className='aspect-auto h-72 w-auto rounded-3xl object-cover object-center shadow-sm shadow-zinc-900/60 dark:shadow-zinc-100/60'
+									className='aspect-auto h-[450px] w-auto rounded-3xl object-cover object-center shadow-sm shadow-zinc-900/60 dark:shadow-zinc-100/60'
 								/>
-								<div className='absolute inset-0 bg-background opacity-10 transition-opacity duration-[450ms] hover:opacity-45 rounded-3xl'></div>
-							</div>
-
-							<div className='flex flex-col mt-12 gap-6'>
-								<Button className='flex-1 w-1/2 mx-auto lg:py-3 bg-transparent bg-gradient-to-tr from-primary to-secondary shadow-zinc-900/60 dark:shadow-zinc-100/60'>
-									Reach Me <ArrowRight />
-								</Button>
-								<Button className='flex-1 w-1/2 mx-auto lg:py-3 bg-transparent bg-gradient-to-tr from-secondary to-primary shadow-sm shadow-zinc-900/60 dark:shadow-zinc-100/60'>
-									My Stuff <ArrowRight />
-								</Button>
+								<AnimatePresence>
+									{isHovered && (
+										<>
+											<motion.div
+												initial='initial'
+												animate='animate'
+												exit='initial'
+												variants={overlayAnimationVariants}
+												className='absolute inset-0 bg-background rounded-3xl z-10'
+											/>
+											<motion.div
+												initial='initial'
+												animate='animate'
+												exit='initial'
+												className='absolute inset-0 flex flex-col justify-center items-center rounded-3xl z-20'
+											>
+												{range.map((_, i) => (
+													<motion.div
+														key={i}
+														custom={i}
+														variants={buttonAnimationVariants}
+														className='w-1/2 mb-4'
+													>
+														<Button className='w-full bg-transparent bg-gradient-to-tr from-primary to-secondary shadow-zinc-900/60 dark:shadow-zinc-100/60'>
+															{i === 0 ? (
+																<>
+																	<ArrowLeft />
+																	Reach Me
+																</>
+															) : (
+																<>
+																	My Stuff
+																	<ArrowRight />
+																</>
+															)}
+														</Button>
+													</motion.div>
+												))}
+											</motion.div>
+										</>
+									)}
+								</AnimatePresence>
 							</div>
 						</div>
 					</div>
