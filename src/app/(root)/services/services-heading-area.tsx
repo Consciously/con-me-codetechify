e81bbaf13acfaee0e4c995daf8341b737465ee5b
Consciously <stefanihle@gmx.net>
2024-07-11 16:49:51 +0200

    refactor services section

---
+++ src/app/(root)/services/services-heading-area.tsx
@@ -0,0 +1,11 @@
+import Section from '@/components/ui/custom-section';
+
+export default function ServicesHeadingArea() {
+	return (
+		<Section.ContentContainer>
+			<h2 className='text-2xl/relaxed md:text-5xl/relaxed font-semibold tracking-tight text-center text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary'>
+				What I Offer
+			</h2>
+		</Section.ContentContainer>
+	);
+}
--- src/app/(root)/services/services-section.tsx
+++ src/app/(root)/services/services-section.tsx
@@ -1,66 +1,18 @@
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import Section from '@/components/ui/custom-section';
-import { SERVICES_DATA } from '@/constants/constants';
-import {
-	Code,
-	Activity,
-	Link2,
-	CheckSquare,
-	Layers,
-	Wrench,
-} from 'lucide-react';
+import Spacer from '@/components/ui/spacer';
+
+import ServicesHeadingArea from './services-heading-area';
+import ServiceOfferArea from './service-offer-area';
 
 export default function ServicesSection() {
 	return (
 		<Section>
-			<Section.GridContainer className='gap-6 my-12 md:my-24 xl:my-48'>
-				<Section.ContentContainer>
-					<h2 className='text-2xl/relaxed md:text-5xl/relaxed font-semibold tracking-tight text-center text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary'>
-						What I Offer
-					</h2>
-				</Section.ContentContainer>
-				<Section.ContentContainer className='mx-auto'>
-					<div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6'>
-						{SERVICES_DATA.map(services => (
-							<Card
-								key={services.id}
-								className='flex-1 w-full border-2 border-primary bg-transparent'
-							>
-								<CardHeader>
-									<CardTitle className='text-lg/relaxed text-center text-balance font-semibold text-primary'>
-										{services.title}
-									</CardTitle>
-								</CardHeader>
-								<CardContent>
-									<div className='grid grid-cols-12 justify-center items-center'>
-										<div className='col-span-4'>
-											{services.icon === 'code' && (
-												<Code className='w-10 h-10 text-secondary' />
-											)}
-											{services.icon === 'activity' && (
-												<Activity className='w-10 h-10 text-secondary' />
-											)}
-											{services.icon === 'link2' && (
-												<Link2 className='w-10 h-10 text-secondary' />
-											)}
-											{services.icon === 'check-square' && (
-												<CheckSquare className='w-10 h-10 text-secondary' />
-											)}
-											{services.icon === 'layers' && (
-												<Layers className='w-10 h-10 text-secondary' />
-											)}
-											{services.icon === 'wrench' && (
-												<Wrench className='w-10 h-10 text-secondary' />
-											)}
-										</div>
-										<div className='col-span-8'>{services.description}</div>
-									</div>
-								</CardContent>
-							</Card>
-						))}
-					</div>
-				</Section.ContentContainer>
-			</Section.GridContainer>
+			<Spacer>
+				<Section.GridContainer className='gap-6'>
+					<ServicesHeadingArea />
+					<ServiceOfferArea />
+				</Section.GridContainer>
+			</Spacer>
 		</Section>
 	);
 }
