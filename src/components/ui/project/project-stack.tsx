8bdcda4ff58c624e2ae00afbe8c11c40610b62f0
Consciously <stefanihle@gmx.net>
2024-07-23 22:54:13 +0200

    layout project item further for bigger screens

--- src/components/ui/project/project-stack.tsx
+++ src/components/ui/project/project-stack.tsx
@@ -8,11 +8,11 @@ export default function ProjectStack({ project }: { project: SelectProject }) {
 				<h4 className='text-xl/relaxed md:text-2xl/relaxed font-semibold tracking-tight text-balance text-center'>
 					Features
 				</h4>
-				<ul className='flex flex-col gap-6 my-6'>
+				<ul className='grid grid-cols-2 gap-3'>
 					{project.features.map(feature => (
 						<li
 							key={project.id}
-							className='text-primary-foreground bg-gradient-to-r from-primary to-secondary p-0.5 md:p-1 text-center'
+							className='col-span-full md:col-span-1 text-primary-foreground bg-gradient-to-r from-primary to-secondary p-0.5 md:p-1 text-center'
 						>
 							{feature}
 						</li>
@@ -23,11 +23,11 @@ export default function ProjectStack({ project }: { project: SelectProject }) {
 				<h4 className='text-xl/relaxed md:text-2xl/relaxed font-semibold tracking-tight text-balance text-center'>
 					Technologies
 				</h4>
-				<ul className='flex flex-col gap-6 my-6'>
+				<ul className='grid grid-cols-2 gap-3'>
 					{project.technologies.map(technology => (
 						<li
 							key={project.id}
-							className='text-primary-foreground bg-gradient-to-r from-secondary to-primary p-0.5 md:p-1 text-center'
+							className='col-span-full md:col-span-1 text-primary-foreground bg-gradient-to-r from-secondary to-primary p-0.5 md:p-1 text-center'
 						>
 							{technology}
 						</li>
