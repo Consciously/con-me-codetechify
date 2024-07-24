5171eb16b700466d1ca51fd1ce3b9c32cc4dce10
Consciously <stefanihle@gmx.net>
2024-07-24 10:23:23 +0200

    change dimensions from next/images from fill to width and height attributes

--- src/components/ui/project/project-images-container.tsx
+++ src/components/ui/project/project-images-container.tsx
@@ -23,28 +23,30 @@ export default function ProjectImagesContainer({
 
 	return (
 		<ProjectStruct.ImagesContainer className={cn('w-full', className)}>
-			<div className='grid grid-cols-6 gap-3 w-full'>
-				<div className='col-span-4 sm:col-span-full'>
-					<ProjectStruct.Image className='w-full h-[256px] relative'>
+			<div className='grid grid-cols-8 gap-3 w-full'>
+				<div className='col-span-6 my-auto'>
+					<ProjectStruct.Image className='w-full h-full'>
 						<Image
 							src={selectedImage}
 							alt={project.title}
-							fill
-							className='object-cover object-center'
+							width={1024}
+							height={1024}
+							className='aspect-square w-full h-auto object-cover object-center'
 						/>
 					</ProjectStruct.Image>
 				</div>
-				<div className='col-span-2 sm:col-span-full grid grid-cols-2 gap-x-3 gap-y-6 sm:gap-y-3 my-auto'>
+				<div className='col-span-2 grid grid-cols-1 gap-3'>
 					{project.images.map((image, idx) => (
 						<div className='col-span-1' key={idx}>
 							<ProjectStruct.Image
-								className='w-full h-[96px] cursor-pointer relative'
+								className='w-full h-full cursor-pointer'
 								onClick={() => handleImageClick(image)}
 							>
 								<Image
 									src={image}
-									fill
-									className='object-cover object-center'
+									width={1024}
+									height={1024}
+									className='aspect-square w-full h-auto object-cover object-center'
 									alt={`Thumbnail ${idx + 1}`}
 								/>
 							</ProjectStruct.Image>
