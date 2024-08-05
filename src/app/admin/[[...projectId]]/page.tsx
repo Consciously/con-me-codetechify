32af16584c94cfb46bf3ebabd2c558645087846e
Consciously <stefanihle@gmx.net>
2024-08-05 15:00:07 +0200

    improve auth process by adding permissions

--- src/app/admin/[[...projectId]]/page.tsx
+++ src/app/admin/[[...projectId]]/page.tsx
@@ -1,8 +1,22 @@
 import ContainerStruct from '@/components/ui/custom-container-layout';
 import H1 from '@/components/ui/h1';
 import DropzoneUploader from '@/app/admin/dropzone-uploader';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { redirect } from 'next/navigation';
 
-export default function AdminPage() {
+export default async function AdminPage() {
+	const { isAuthenticated, getPermission } = getKindeServerSession();
+	const isUserAuthenticated = await isAuthenticated();
+	const hasAdminPermission = await getPermission('access:admin_dashboard');
+
+	if (!isUserAuthenticated) {
+		redirect('/api/auth/login');
+	}
+
+	if (!hasAdminPermission?.isGranted) {
+		redirect('/');
+	}
+
 	return (
 		<ContainerStruct className='my-12 md:my-24 xl:my-48'>
 			<ContainerStruct.Layout className='gap-6'>
