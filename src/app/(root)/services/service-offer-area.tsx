f84c75406cb11d06fed4f133843f360c3f4735a5
Consciously <stefanihle@gmx.net>
2024-08-20 16:25:15 +0200

    refactor services-offer-area

--- src/app/(root)/services/service-offer-area.tsx
+++ src/app/(root)/services/service-offer-area.tsx
@@ -1,4 +1,3 @@
-import ContainerStruct from '@/components/ui/custom-container-layout';
 import { SERVICES_DATA } from '@/constants/constants';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import {
@@ -9,49 +8,51 @@ import {
 	Layers,
 	Wrench,
 } from 'lucide-react';
+import Block from '@/components/ui/custom-block-structure';
 
 export default function ServiceOfferArea() {
 	return (
-		<ContainerStruct.Content className='mx-auto'>
-			<div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6'>
-				{SERVICES_DATA.map(services => (
-					<Card
-						key={services.id}
-						className='flex-1 w-full border-2 border-primary bg-transparent'
-					>
-						<CardHeader>
-							<CardTitle className='text-lg/relaxed text-center text-balance font-semibold text-primary'>
-								{services.title}
-							</CardTitle>
-						</CardHeader>
-						<CardContent>
-							<div className='grid grid-cols-12 justify-center items-center'>
-								<div className='col-span-4'>
-									{services.icon === 'code' && (
-										<Code className='w-10 h-10 text-secondary' />
-									)}
-									{services.icon === 'activity' && (
-										<Activity className='w-10 h-10 text-secondary' />
-									)}
-									{services.icon === 'link2' && (
-										<Link2 className='w-10 h-10 text-secondary' />
-									)}
-									{services.icon === 'check-square' && (
-										<CheckSquare className='w-10 h-10 text-secondary' />
-									)}
-									{services.icon === 'layers' && (
-										<Layers className='w-10 h-10 text-secondary' />
-									)}
-									{services.icon === 'wrench' && (
-										<Wrench className='w-10 h-10 text-secondary' />
-									)}
-								</div>
-								<div className='col-span-8'>{services.description}</div>
-							</div>
-						</CardContent>
-					</Card>
-				))}
-			</div>
-		</ContainerStruct.Content>
+		<Block>
+			<Block.Item>
+				<Block.ContentGroup>
+					{SERVICES_DATA.map(services => (
+						<Block.Content key={services.id} className='min-h-48'>
+							<Card className='flex-1 w-full border-2 border-primary bg-transparent'>
+								<CardHeader>
+									<CardTitle className='text-clamp-md leading-relaxed text-center text-balance font-semibold text-primary'>
+										{services.title}
+									</CardTitle>
+								</CardHeader>
+								<CardContent>
+									<div className='grid grid-cols-12 justify-center items-center'>
+										<div className='col-span-4'>
+											{services.icon === 'code' && (
+												<Code className='w-10 h-10 text-secondary' />
+											)}
+											{services.icon === 'activity' && (
+												<Activity className='w-10 h-10 text-secondary' />
+											)}
+											{services.icon === 'link2' && (
+												<Link2 className='w-10 h-10 text-secondary' />
+											)}
+											{services.icon === 'check-square' && (
+												<CheckSquare className='w-10 h-10 text-secondary' />
+											)}
+											{services.icon === 'layers' && (
+												<Layers className='w-10 h-10 text-secondary' />
+											)}
+											{services.icon === 'wrench' && (
+												<Wrench className='w-10 h-10 text-secondary' />
+											)}
+										</div>
+										<div className='col-span-8'>{services.description}</div>
+									</div>
+								</CardContent>
+							</Card>
+						</Block.Content>
+					))}
+				</Block.ContentGroup>
+			</Block.Item>
+		</Block>
 	);
 }
